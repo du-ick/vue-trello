a5f87cc7755b5c686637ac7c7ce743d6b748b7e7
HJsong <seaalgae.0@gmail.com>
2023-04-23 15:17:12 +0900

    스토어 개선

--- src/store/index.js
+++ src/store/index.js
@@ -1,39 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import * as api from '../api'
+import state from './state';
+import getters from './getters';
+import mutations from './mutations';
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-  state: {
-    token: null,
-    isAddBoard: false,
-    boards: [],
-  },
-  getters: {
-    isAuth(state) {
-      return !!state.token
-    }
-  },
-  mutations: {
-    LOGIN (state, token) {
-      if (!token) return 
-      state.token = token
-      localStorage.setItem('token', token)
-      api.setAuthInHeader(token)
-    },
-    LOGOUT (state) {
-      state.token = null
-      delete localStorage.token
-      api.setAuthInHeader(null)
-    },
-    SET_IS_ADD_BOARD (state, toggle) {
-      state.isAddBoard = toggle
-    },
-    SET_BOARDS (state, boards) {
-      state.boards = boards
-    }
-  },
+  state,
+  getters,
+  mutations,
   actions: {
     LOGIN ({commit}, {email, password}) {
       return api.auth.login(email, password)
